7806c227595ac3ac719b8575051ed5cf37575888
Sagar Lodhe <sagar1990ku@gmail.com>
2024-09-24 22:10:32 +0530

    Pointer

---
+++ Pointers/01.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=2;
+    cout<<x<<endl;
+    int* p = &x;
+    cout<<"Address of x"<<endl;
+    cout<<p<<endl;
+    cout<<"Access value of x using p"<<endl;
+    cout<<*p<<endl;
+
+}
